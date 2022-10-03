b63a18c17ade50f65e33c7d778f61e6fd0090b55
Felipe Ittner <felipeittner5@gmail.com>
2022-10-03 16:47:42 -0300

    Add project files.

--- MVCPresentationLayer/Views/Config.java
+++ MVCPresentationLayer/Views/Config.java
@@ -1,21 +1,16 @@
 package MVCPresentationLayer.Views;
 import javax.swing.JOptionPane;
 
-import MVCPresentationLayer.Controllers.ClientController;
-
 public class Config {
     public static void show(){
-        ClientController C = new ClientController();
         String Options = "Escolha uma opção\n\n" +
         "1 - Alterar Nome\n" +
         "2 - Alterar Email\n" +
         "3 - Alterar Senha\n" +
-        "0 - Voltar\n\n" +
-        ", Perfil do Usuário";
-        int userProfileOption = 0;
+        "0 - Voltar\n\n";
+        int userProfileOption = 3;
         while (userProfileOption != 0) {
-            userProfileOption = Integer.parseInt(JOptionPane.showInputDialog(null, 
-            Options, JOptionPane.QUESTION_MESSAGE));
+            userProfileOption = Integer.parseInt(JOptionPane.showInputDialog(null, Options));
 
             if (userProfileOption == 0)
             switch (userProfileOption) {
